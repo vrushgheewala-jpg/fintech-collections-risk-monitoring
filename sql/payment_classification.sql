d6097316350e019e6db627a420ffa04f5b7089af
vrushgheewala-jpg <vrush.gheewala@gmail.com>
2026-02-25 14:33:35 +0530

    Revise payment classification SQL query
    Updated the SQL query to include invoice amount and issue date, and modified the payment date handling in the calculations.

--- sql/payment_classification.sql
+++ sql/payment_classification.sql
@@ -1,28 +1,36 @@
 SELECT
     i.invoice_id,
     i.customer_id,
+    i.amount,
+    i.issue_date,
     i.due_date,
     p.payment_date,
 
+    -- payment delay
     CASE
         WHEN p.payment_date IS NULL
             THEN DATE_DIFF(CURRENT_DATE(), i.due_date, DAY)
-        ELSE DATE_DIFF(p.payment_date, i.due_date, DAY)
+        ELSE DATE_DIFF(DATE(p.payment_date), i.due_date, DAY)
     END AS days_late,
 
+    -- payment category
     CASE
-        WHEN p.payment_date IS NULL AND DATE_DIFF(CURRENT_DATE(), i.due_date, DAY) > 30
+        WHEN p.payment_date IS NULL
+             AND DATE_DIFF(CURRENT_DATE(), i.due_date, DAY) > 30
             THEN "HIGH RISK - UNPAID"
+
         WHEN p.payment_date IS NULL
             THEN "UNPAID"
-        WHEN DATE_DIFF(p.payment_date, i.due_date, DAY) > 30
+
+        WHEN DATE_DIFF(DATE(p.payment_date), i.due_date, DAY) > 30
             THEN "PAID VERY LATE"
-        WHEN DATE_DIFF(p.payment_date, i.due_date, DAY) > 0
+
+        WHEN DATE_DIFF(DATE(p.payment_date), i.due_date, DAY) > 0
             THEN "PAID LATE"
+
         ELSE "ON TIME / EARLY"
     END AS payment_category
 
 FROM `my-first-project-485906.fintech_project.invoices` i
 LEFT JOIN `my-first-project-485906.fintech_project.payments` p
 ON i.invoice_id = p.invoice_id
-LIMIT 50
